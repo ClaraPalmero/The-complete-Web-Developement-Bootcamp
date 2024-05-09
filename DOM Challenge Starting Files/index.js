20bb38eca05b3408dcc324a363fe0244e7cd9bc7
Clara Palmero <clarapalmero@email.com>
2024-05-09 13:51:55 +0200

    DOM challange

---
+++ DOM Challenge Starting Files/index.js
@@ -0,0 +1,16 @@
+ document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "3rd";
+
+ document.querySelector("li a").style.color = "red";
+
+ document.querySelector("button").style.backgroundColor = "yellow";
+
+ document.querySelector("h1").classList.add("huge");
+
+ document.querySelector("h1").innerHTML = "<em>Hola</em>";
+
+
+ document.querySelector("ul").lastElementChild.innerHTML = "Third";
+
+ document.querySelector(".list a").style.color = "green";
+
+ document.querySelector("a").setAttribute("href", "https://miro.com/app/board/uXjVKLfgcTk=/");
