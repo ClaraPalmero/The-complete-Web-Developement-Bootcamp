2807df8131be5a06e3bba63702100b0f07f1e45d
Clara Palmero <clarapalmero@email.com>
2024-05-14 10:29:31 +0200

    dice challange updated

--- Dicee+Challenge+-+Starting+Files/app.js
+++ Dicee+Challenge+-+Starting+Files/app.js
@@ -1,23 +1,33 @@
-let randomNumber1 = Math.floor(Math.random() * 6) + 1;
-let randomImg1 = `./images/dice${randomNumber1}.png`;
-document.querySelectorAll("img")[0].setAttribute("src", randomImg1);
+// let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+// let randomImg1 = `./images/dice${randomNumber1}.png`;
+// document.querySelectorAll("img")[0].setAttribute("src", randomImg1);
 
-let randomNumber2 = Math.floor(Math.random() * 6) + 1;
-let randomImage2 = `./images/dice${randomNumber2}.png`;
-document.querySelectorAll("img")[1].setAttribute("src", randomImage2);
+// let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+// let randomImage2 = `./images/dice${randomNumber2}.png`;
+// document.querySelectorAll("img")[1].setAttribute("src", randomImage2);
+
+// if(randomNumber1 < randomNumber2){
+//     document.querySelector("h1").textContent = "🚩 Player2 wins!";
+// }else if(randomNumber1 > randomNumber2){
+//     document.querySelector("h1").textContent = "🚩 Player1 wins!";
+// }else{
+//     document.querySelector("h1").textContent = "🚩 Draw";
+// }
+
+const randomNumber1 = Math.floor(Math.random() * 6) + 1;
+const randomImage1 = `./images/dice${randomNumber1}.png`;
+document.querySelector(".img1").setAttribute("src", randomImage1)
+
+const randomNumber2 = Math.floor(Math.random() * 6) + 1;
+const randomImage2 = `./images/dice${randomNumber2}.png`;
+document.querySelector(".img2").setAttribute("src", randomImage2)
 
 if(randomNumber1 < randomNumber2){
-    document.querySelector("h1").textContent = "🚩 Player2 wins!";
+    document.querySelector("h1").innerHTML = "🏆 Player2 wins";
 }else if(randomNumber1 > randomNumber2){
-    document.querySelector("h1").textContent = "🚩 Player1 wins!";
+    document.querySelector("h1").innerHTML = "🏆 Player1 wins";
 }else{
-    document.querySelector("h1").textContent = "🚩 Draw";
+    document.querySelector("h1").innerHTML = "Draw";
 }
-
-
-
-
-
-
 
 
